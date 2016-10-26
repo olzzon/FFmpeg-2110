20bdf00c8e15081d52e78592255751967d3f19c5
Damien Riegel <damien.riegel@savoirfairelinux.com>
2016-10-26 15:54:35 -0400

    ci: add cqfd support
    Cqfd allows fast compile and CI integration. Upstream cqfd version is
    1e78ddd1.
    
    With the provided Dockerfile and shell script, cqfd will build FFmpeg
    and it dependencies in a Centos 7 environment.
    
    Issue: #1059
    Change-Id: I344008982cf6ed5b5a2fa140d6e20dca2a62fa35

---
+++ .cqfd/docker/build-ffmpeg-dependencies.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Compile & Install FFMPEG libraries
+
+set -euo pipefail
+
+echo "/usr/local/lib" >/etc/ld.so.conf.d/libc.conf
+
+if [ -z "$PREFIX" ]; then
+	echo 'PREFIX was not set in Dockerfile. Aborting.'
+	exit 1
+fi
+
+export MAKEFLAGS="-j$[$(nproc) + 1]"
+
+# yasm
+DIR=$(mktemp -d)
+cd $DIR/
+curl -s http://www.tortall.net/projects/yasm/releases/yasm-$YASM_VERSION.tar.gz |
+	tar zxvf - -C .
+cd $DIR/yasm-$YASM_VERSION/
+./configure --prefix="$PREFIX" --bindir="$PREFIX/bin" --docdir=$DIR -mandir=$DIR
+make
+make install
+make distclean
+rm -rf $DIR
+
+# x264
+DIR=$(mktemp -d)
+cd $DIR/
+git clone -b stable  --single-branch http://git.videolan.org/git/x264.git
+cd x264/
+./configure --prefix="$PREFIX" --bindir="$PREFIX/bin" --enable-static
+make
+make install
+make distclean
+rm -rf $DIR
+
+# fdk-aac
+DIR=$(mktemp -d)
+cd $DIR/
+curl -s https://codeload.github.com/mstorsjo/fdk-aac/tar.gz/v$FDKAAC_VERSION |
+	tar zxvf - -C .
+cd fdk-aac-$FDKAAC_VERSION/
+autoreconf -fiv
+./configure --prefix="$PREFIX" --disable-shared
+make
+make install
+make distclean
+rm -rf $DIR
